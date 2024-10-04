28150232fbc68f497351c0ee2aa02ab7f36b661e
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-04 18:48:40 +0200

    added form select and filtering logic

--- snack_4/index.php
+++ snack_4/index.php
@@ -2,13 +2,15 @@
 // File with data
 include 'Classi-per-131.php';
 
+// Define properties for a students in a indexe array
 $students_properties = array_keys($classi["Classe 1A"][0]);
 // var_dump(array_keys($students_properties)[0]);
 
+// Initialize a variable to store filter result
 $filtered_classes = [];
 
-// Filter classes per student's school average above 6
-foreach ($classi as $class_name => $class) {
+// Filter students per student's school average above 6
+/* foreach ($classi as $class_name => $class) {
   $filtered_class = [];
   foreach ($class as $student) {
     if ($student["voto_medio"] >= 6) {
@@ -17,11 +19,33 @@ foreach ($classi as $class_name => $class) {
   }
   $filtered_classes[$class_name] = $filtered_class;
 }
+ */
 
-if(isset($filtered_classes)) {
-  $classi = $filtered_classes;
+// Initialize a maximum average to use it whes set from form
+$max_average;
+// Filter students per school average under selected mean vote
+if (isset($_GET['max-average'])) {
+  $max_average = $_GET['max-average'];
+  // echo '$max_average vale ' . $max_average;
+  foreach ($classi as $class_name => $class) {
+    $filtered_class = [];
+    foreach ($class as $student) {
+      if ($student["voto_medio"] <= $max_average) {
+        $filtered_class[] = $student;
+      }
+    }
+    $filtered_classes[$class_name] = $filtered_class;
+  }
 }
+
+// Set var $classi equal to filter result ($filtered_class)
+if(isset($filtered_classes) && !empty($filtered_classes)) {
+  // echo 'if di settaggio $classi';
+  $classi = $filtered_classes;
+} 
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,9 +57,26 @@ if(isset($filtered_classes)) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-  <main>
-    <div class="container-md">
-      <h1 class="text-center py-2 mb-3">Students of coding language</h1>
+  <h1 class="text-center py-2 mb-3">Students of coding language</h1>
+  
+  <div class="container-md">
+    <!-- Form -->
+    <form action="" class="form-control py-2 my-4 w-50 m-auto">
+      <label class="mb-1" for="max-average">Max school average visible</label><br>
+      <select class="form-select w-25" name="max-average" id="max-average">
+        <?php for($i = 1; $i <= 10; $i++) { ?>
+        <option name="<?= $i; ?>" value="<?= $i; ?>">
+        <?= $i; ?>
+        </option>
+        <?php }; ?>
+      </select>
+
+      <!-- Buttons -->
+      <button class="btn btn-info mt-4">Submit</button>
+    </form>
+
+    <!-- Page contents -->
+    <main>
       <section class="classes-cards">
         <div class="row">
           <?php foreach($classi as $class_name => $class_array) { ?>
@@ -80,7 +121,6 @@ if(isset($filtered_classes)) {
           <?php } ?>
         </div>
       </section>
-    </div>
-  </main>
+    </main>
 </body>
 </html>
