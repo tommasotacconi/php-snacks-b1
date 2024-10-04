e6397a6b97dfce81d7fbb0cfbd6bb2f48e49178f
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-04 12:53:48 +0200

    added first div logic without bg

--- snack_3/index.php
+++ snack_3/index.php
@@ -31,15 +31,19 @@ $db = [
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <!-- Link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
   <div class="grey-div">
     <h1>Teachers:</h1>
     <ul>
-      <?php foreach ($teachers as $teacher) { ?>
+      <?php
+      $teachers = $db['teachers'];
+      foreach ($teachers as $teacher) { ?>
       <li>
-        <span class="info">Teacher's firstname:</span>
-        <span class="info">Teacher's surname:</span>
+        <span class="info">Teacher's firstname:</span><?=  $teacher['name']; ?>
+        <span class="info">Teacher's surname:</span><?=  $teacher['lastname']; ?>
       </li>
       <?php } ?>
     </ul>
