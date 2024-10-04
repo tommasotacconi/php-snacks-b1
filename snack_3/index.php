5a78f564b9a1d73d7d7fb745cb7b895999bf3cbb
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-04 13:01:52 +0200

    added complete dinamic fill and style

--- snack_3/index.php
+++ snack_3/index.php
@@ -35,19 +35,34 @@ $db = [
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-  <div class="grey-div">
-    <h1>Teachers:</h1>
-    <ul>
-      <?php
-      $teachers = $db['teachers'];
-      foreach ($teachers as $teacher) { ?>
-      <li>
-        <span class="info">Teacher's firstname:</span><?=  $teacher['name']; ?>
-        <span class="info">Teacher's surname:</span><?=  $teacher['lastname']; ?>
-      </li>
-      <?php } ?>
-    </ul>
-  </div>
+  <main class="p-3">
+    <div class="bg-secondary">
+      <h1>Teachers:</h1>
+      <ul>
+        <?php
+        $teachers = $db['teachers'];
+        foreach ($teachers as $teacher) { ?>
+        <li>
+          <span class="info">Teacher's firstname:</span> <?=  $teacher['name']; ?><br>
+          <span class="info">Teacher's surname:</span> <?=  $teacher['lastname']; ?>
+        </li>
+        <?php } ?>
+      </ul>
+    </div>
+    <div class="bg-success-subtle">
+      <h1>Pms:</h1>
+      <ul>
+        <?php
+        $pms = $db['pm'];
+        foreach ($pms as $pm) { ?>
+        <li>
+          <span class="info">Pm's firstname:</span> <?=  $pm['name']; ?><br>
+          <span class="info">Pm's surname:</span> <?=  $pm['lastname']; ?>
+        </li>
+        <?php } ?>
+      </ul>
+    </div>
+  </main>
   
 </body>
 </html>
