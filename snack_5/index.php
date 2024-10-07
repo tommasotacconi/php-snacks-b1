54c503138fe027cca570c8e086a5feaa01f15156
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-07 12:18:13 +0200

    palindrome php form

---
+++ snack_5/index.php
@@ -0,0 +1,24 @@
+<?php 
+; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+  <main>
+    <h1>Palindrome words checker</h1>
+    <form action="" method="$_GET">
+      <label for="palidrome_controll">Word to check</label>
+      <input type="text">
+    </form>
+    <p></p>
+  </main>  
+  </div> 
+</body>
+</html>
